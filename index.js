f997ae3285caca586f00920f667109a1c967c98d
Tiger Ji <tiger.j540@gmail.com>
2022-06-20 20:09:53 -0500

    last step before css fw

--- index.js
+++ index.js
@@ -101,13 +101,40 @@ const blurHome = () => {
 
 const showExperienceDescription = (organization) => {
     if (organization == "iCode") {
-        document.getElementById("experience-description").innerHTML = "icode";
+        document.getElementById("experience-description").innerHTML = `
+        <p style="margin-top: 0px;">
+            Instructor @ iCode
+        </p>
+        <p> August 2020 — July 2021 </p>
+        <ul>
+            <li> Planned and held open house demos for 100+ prospective students and families over the year </li>
+            <li> Taught weekly classes for various age ranges about coding fundamentals in Scratch, introductory algorithms with LEGO EV3 robots, and basic web development in HTML </li>
+            <li> Led “holiday camps” for 40+ hours/week on subjects from programming drones in Python to Game Development in Construct 3 to working with Raspberry Pi and circuits </li>
+        </ul>`;
     }
     else if (organization == "EnLiving Design") {
-        document.getElementById("experience-description").innerHTML = "enliving design";
+        document.getElementById("experience-description").innerHTML = `
+        <p style="margin-top: 0px;">
+            Research Assistant @ EnLiving Design
+        </p>
+        <p> May 2020 — August 2020 </p>
+        <ul>
+            <li> Read 100+ pages of literature to learn about the history and utility of depth cameras in helping occupational therapists more accurately assess clinical sitting body balance </li>
+            <li> Coded a program in C# to track and calculate joint imovement patterns using the Intel RealSense skeleton tracking SDK as an update to the previous code written in Processing for the discontinued Microsoft Kinect line </li>
+        </ul>`;
     }
     else {
-        document.getElementById("experience-description").innerHTML = "google cssi";
+        document.getElementById("experience-description").innerHTML = `
+        <p style="margin-top: 0px;">
+            Computer Science Summer Institute @ Google
+        </p>
+        <p> July 2021 — August 2021 </p>
+        <ul>
+            <li> Completed a project-based curriculum for HTML, CSS, JS, and Firebase taught by Google developers </li>
+            <li> Configured 10+ web development projects to practice concepts including object-oriented programming, effective UI/UX design, APIs, authorization/authentication, and Git </li>
+            <li> Implemented a personalized “university student page” web application (see Featured Projects for more information) </li>
+            <li> Delivered a final presentation of our product to Google employees and community leaders </li>
+        </ul>`;
     }
 
     console.log(organization);
