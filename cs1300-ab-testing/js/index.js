bbae5b518da75c6862d2753058113680e60454ec
Tiger Ji <tiger.j540@gmail.com>
2024-03-14 00:50:06 -0400

    try 1300 abtesting again

---
+++ cs1300-ab-testing/js/index.js
@@ -0,0 +1,15 @@
+const showSynonyms = () => {
+  document.getElementById("options-list").classList.remove("hidden");
+}
+
+const hideSynonyms = () => {
+  document.getElementById("options-list").classList.add("hidden");
+}
+
+const showOptions = () => {
+  document.getElementById("suggestions-list").classList.remove("hidden");
+}
+
+const hideOptions = () => {
+  document.getElementById("suggestions-list").classList.add("hidden");
+}
